2afced731337aad7339f8cb201f9a3fde2f5bc65
HoangBui <s3818202@rmit.edu.vn>
2022-07-28 13:51:17 +0700

    Initial Commit

---
+++ TFTWikiApp/TFTWikiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TFTWikiAppApp.swift
+//  TFTWikiApp
+//
+//  Created by Hoang Bui  on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TFTWikiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
